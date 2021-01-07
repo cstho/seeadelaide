87512236104de05b279470dc26f2b012040498c5
Crazyzooka <50309931+Crazyzooka@users.noreply.github.com>
2021-01-08 03:13:25 +1030

    seperated googlesignin from app

--- App.js
+++ App.js
@@ -1,39 +1,12 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import * as Google from 'expo-google-app-auth';
-
-import * as Test from './modules/testfunc.js';
-
-const tempstr = "empty"
-
-// First- obtain access token from Expo's Google API
-async function googleSignIn() {
-
-  const { type, accessToken, user } = await Google.logInAsync({
-    androidClientId: '1028854562585-mmqdqdtqrit6e8e0a01sro57tkd9mob8.apps.googleusercontent.com',
-    scopes: ['profile', 'email'],
-  });
-
-  if (type === 'success') {
-    // Then you can use the Google REST API
-    // let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
-    //   headers: { Authorization: `Bearer ${accessToken}` },
-    // });
-    Test.testFunc();
-    alert("success!");
-    return;
-  }
-  else {
-    alert("failed.");
-    return;
-  }
-}
+import * as SignIn from './modules/googlesignin.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => googleSignIn()}><Text>Sign-in</Text></TouchableOpacity>
+      <TouchableOpacity onPress={() => SignIn.googleSignIn()}><Text>Sign-in</Text></TouchableOpacity>
     </View>
   );
 }
