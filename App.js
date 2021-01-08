1c58006e79300a9de555bb08dd10dafc63351472
PhucNguyen1501 <62129450+PhucNguyen1501@users.noreply.github.com>
2021-01-08 21:33:24 +1030

    login Screen added

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 // import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import * as SignIn from './modules/googlesignin.js';
+// import React from 'react';
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+// import * as SignIn from './modules/googlesignin.js';
+import loginScreen from './modules/loginScreen.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => SignIn.googleSignIn()}><Text>Sign-in</Text></TouchableOpacity>
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <TouchableOpacity onPress={() => SignIn.googleSignIn()}><Text>Sign-in</Text></TouchableOpacity>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default loginScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
