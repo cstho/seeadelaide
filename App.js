1731b8d4e1202edcc031c50b9e1d5be9b4530f65
Crazyzooka <50309931+Crazyzooka@users.noreply.github.com>
2021-01-13 01:18:29 +1030

    worked out how to use classes, sign in button changes text now

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import * as React from 'react';
-import * as functions from './ScreenContainer'
-import { NavigationContainer } from '@react-navigation/native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { SignIn } from './modules/googlesignin.js';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+//import loginScreen from './modules/loginScreen.js'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <functions.MyTabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Press below:</Text>
+      <SignIn/>
+    </View>
   );
 }
+
+//export default loginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- modules/googlesignin.js
+++ modules/googlesignin.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as Google from 'expo-google-app-auth';
 import * as Test from './testfunc.js';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 // First- obtain access token from Expo's Google API
-export const googleSignIn = async () => {
 
-  const { type, accessToken, user } = await Google.logInAsync({
-    androidClientId: '1028854562585-mmqdqdtqrit6e8e0a01sro57tkd9mob8.apps.googleusercontent.com',
-    iosClientId: '1028854562585-9pmab1onorsna0i3ariuuab2pmoaakaq.apps.googleusercontent.com',
-    scopes: ['profile', 'email'],
-  });
+export class SignIn extends Component {
 
-  if (type === 'success') {
-    // Then you can use the Google REST API
-    // let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
-    //   headers: { Authorization: `Bearer ${accessToken}` },
-    // });
-    Test.testFunc();
-    alert("success!");
-    return;
+  state = {
+    logged: 'Signed in'
   }
-  else {
-    alert("failed.");
-    return;
+
+  googleSignIn = async () => {
+    if (this.state.logged != 'Signed in')
+    {
+      const { type, accessToken, user } = await Google.logInAsync({
+        androidClientId: '1028854562585-mmqdqdtqrit6e8e0a01sro57tkd9mob8.apps.googleusercontent.com',
+        iosClientId: '1028854562585-9pmab1onorsna0i3ariuuab2pmoaakaq.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+      });
+
+      if (type === 'success') {
+        // Then you can use the Google REST API
+        // let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
+        //   headers: { Authorization: `Bearer ${accessToken}` },
+        // });
+        //Test.testFunc();
+        alert("success!");
+        this.setState({
+          logged: 'Signed in'
+        });
+        return;
+      }
+      else {
+        alert("failed.");
+        return;
+      }
+    }
+    else {
+      alert("already logged in");
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <TouchableOpacity onPress={() => this.googleSignIn()}><Text>{this.state.logged}</Text></TouchableOpacity>
+      </View>
+    )
   }
 }
